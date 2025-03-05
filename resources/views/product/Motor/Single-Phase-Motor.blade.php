f7320265e367eb9fe35be4d44b2f4672866a3715
Md. Shumon Mia <shomoncmt@gmail.com>
2025-03-05 15:26:25 +0600

    update website

--- resources/views/product/Motor/Single-Phase-Motor.blade.php
+++ resources/views/product/Motor/Single-Phase-Motor.blade.php
@@ -1,38 +1,129 @@
 @include('master.header')
 
+
+
+
+
+<section class="diff-offer-wrapper">
 <div class="container">
     <div class="row">
-        <div class="col-md-4">
+        <div class="col-md-4" style="margin-top: 50px;">
             <div class="img-responsive zoom_img_effect post-image view image_hover">
-            <img src= "{{ asset('img/product/Dynamic-pump.jpg')}}" alt="">
+            <img src= "{{ asset('img/product/Dynamic-pump.jpg')}}" alt="" >
             </div>
-            
         </div>
-        <div class="col-md-8" style="margin-top: 50px;">
+        <div class="col-md-4" style="margin-top: 50px;">
             <h2>High Efficient Three-Phase Asynchronous Motor </h2>
-            <span>
-            <i class="bi bi-star-fill"></i>
-            </span>
-            <span>
-            <i class="bi bi-star-fill"></i>
-            </span>
-            <span>
-            <i class="bi bi-star-fill"></i>
-            </span>
-            <span>
-            <i class="bi bi-star-fill"></i>
-            </span>
-            <span>
-            <i class="bi bi-star-half"></i>
-            </span>
-            <p>IE3 efficiency level three-phase asynchronous motor.<br>
-Power range 0.75kw ~ 315kw (Customizable).<br>
-Voltage: 380V (Customizable).<br>
-Frequency: 50Hz (or 60Hz)<br>
-We also have YE2/YE3/YE4.</p>
+            <span><i class="bi bi-star-fill"></i></span><span><i class="bi bi-star-fill"></i></span><span><i class="bi bi-star-fill"></i></span><span><i class="bi bi-star-fill"></i></span><span><i class="bi bi-star-half"></i></span>
+            <p>IE3 efficiency level three-phase asynchronous motor.</p>
+            <p>Power range 0.75kw ~ 315kw (Customizable).</p>
+            <p>Voltage: 380V (Customizable).</p>
+            <button type="button" class="btn btn-outline-success" > Inquire Now </button> <button type="button" class="btn btn-outline-success"> Next Product </button>
+
         </div>
     </div>
+    <div class="row ">
+        <h2 style="margin-bottom:10px; margin-top:25px; text-align: center; ">Product Details</h2>
+        <span><button type="button" class="btn btn-outline-success" > Inquire Now </button> <button type="button" class="btn btn-outline-success"> Next Product </button><button type="button" class="btn btn-outline-success" > Inquire Now </button> <button type="button" class="btn btn-outline-success"> Next Product </button></span>
+    </div>
+
+
+
+
+        <div class="row ">
+        <h2 style="margin-bottom:10px; margin-top:25px">Related Product</h2> 
+        </div>
+        <div class="row">
+          <div class="col-md-3 col-sm-8 service-info">
+            <div class="item"><a href={{Route('electricmotor')}} class="post-image view image_hover">			<img src="img/product/Dynamic-Motor.png" alt="" class="img-responsive zoom_img_effect"></a><a href={{Route('electricmotor')}}>
+                <h4>Dynamic Motor</h4></a>
+              <p style="text-align:justify" >Dynamic Motors strives to find you the best deals in the pre-owned vehicle market. As a company, we have years of experience and expertise.</p>
+              <h6><a href={{Route('electricmotor')}}>Read more</a></h6>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-8 service-info">
+            <div class="item"><a href={{Route('electricmotor')}} class="post-image view image_hover">			<img src="img/product/Dynamic-Motor.png" alt="" class="img-responsive zoom_img_effect"></a><a href={{Route('electricmotor')}}>
+                <h4>Dynamic Motor</h4></a>
+              <p style="text-align:justify" >Dynamic Motors strives to find you the best deals in the pre-owned vehicle market. As a company, we have years of experience and expertise.</p>
+              <h6><a href={{Route('electricmotor')}}>Read more</a></h6>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-8 service-info">
+            <div class="item"><a href={{Route('roandufmembrane')}} class="post-image view image_hover"><img src="img/product/Dynamic-Ro-membrane.jpg" alt="" class="img-responsive zoom_img_effect"></a><a href={{Route('roandufmembrane')}}>
+                <h4>Dynamic RO Membrane</h4></a>
+              <p style="text-align:justify">There are many applications for RO, including water and wastewater treatment, food and beverage processing, and pharmaceutical production.</p>
+              <h6><a href={{Route('roandufmembrane')}}>Read more</a></h6>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-8 service-info">
+            <div class="item"><a href={{Route('industrialpump')}} class="post-image view image_hover">		<img src="img/product/Dynamic-pump.jpg" alt="" class="img-responsive zoom_img_effect"></a><a href={{Route('industrialpump')}}>
+                <h4>Dynamic Pump</h4></a>
+              <p style="text-align:justify">Dynamic pumps use kinetic energy to move liquids, like water and oil, from a lower to a higher pressure.</p>
+              <h6><a href={{Route('industrialpump')}}>Read more</a></h6>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-3 col-sm-8 service-info">
+            <div class="item"><a href={{Route('blower')}} class="post-image view image_hover"><img src="img/product/Dynamic-Blower.jpg" alt="" class="img-responsive zoom_img_effect"></a><a href={{Route('blower')}}>
+                <h4>Dynamic Blower</h4></a>
+              <p style="text-align:justify">Roots blowers operate by trapping a fixed amount of fluid or gas and expelling it against the system's pressure.</p>
+              <h6><a href={{Route('blower')}}>Read more</a></h6>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-8 service-info">
+            <div class="item"><a href={{Route('frpvessel')}} class="post-image view image_hover"><img src="img/product/FRP-Vessel.jpg" alt="" class="img-responsive zoom_img_effect"></a><a href={{Route('frpvessel')}}>
+                <h4>Dynamic Vessel</h4></a>
+              <p style="text-align:justify">FRP is an updated composite material used for chemical plant appliances and equipment, including vessels and tanks.</p>
+              <h6><a href={{Route('frpvessel')}}>Read more</a></h6>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-8 service-info">
+            <div class="item"><a href={{Route('electromageneticflowmeter')}} class="post-image view image_hover"><img src="img/product/Flow-meter.jpg" alt="" class="img-responsive zoom_img_effect"></a><a href={{Route('electromageneticflowmeter')}}>
+                <h4>Dynamic Flow Meter</h4></a>
+              <p style="text-align:justify">Dynamic Flow Computers is one of the main global providers of advanced flow measurement solutions.</p>
+              <h6><a href={{Route('electromageneticflowmeter')}}>Read more</a></h6>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-8 service-info">
+            <div class="item"><a href={{Route('electromageneticflowmeter')}} class="post-image view image_hover"><img src="img/product/Flow-meter.jpg" alt="" class="img-responsive zoom_img_effect"></a><a href={{Route('electromageneticflowmeter')}}>
+                <h4>Dynamic Flow Meter</h4></a>
+              <p style="text-align:justify">Dynamic Flow Computers is one of the main global providers of advanced flow measurement solutions.</p>
+              <h6><a href={{Route('electromageneticflowmeter')}}>Read more</a></h6>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-3 col-sm-8 service-info">
+            <div class="item"><a href={{Route('roandufmembrane')}} class="post-image view image_hover"><img src="img/product/RO Antiscalant.jpg" alt="" class="img-responsive zoom_img_effect"></a><a href={{Route('roandufmembrane')}}>
+                <h4>Dynamic RO Antiscalant</h4></a>
+              <p style="text-align:justify">Antiscalant is a specialized chemical treatment that is used in reverse osmosis (RO) plants to prevent scaling and fouling on the membrane surface.</p>
+              <h6><a href={{Route('roandufmembrane')}}>Read more</a></h6>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-8 service-info">
+            <div class="item"><a href={{Route('watertreatmentaccessories')}} class="post-image view image_hover"><img src="img/product/Flocculants & Dechlorinator.jpg" alt="" class="img-responsive zoom_img_effect"></a><a href={{Route('watertreatmentaccessories')}}>
+                <h4>Flocculants & Dechlorinator</h4></a>
+              <p style="text-align:justify">Flocculants are water-soluble polymers that clump fine particles in water, while dechlorinators remove chlorine from water.</p>
+              <h6><a href={{Route('watertreatmentaccessories')}}>Read more</a></h6>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-8 service-info">
+            <div class="item"><a href={{Route('watertreatmentchemicalsandmedia')}} class="post-image view image_hover"><img src="img/product/Biocide & Membrane Cleaner.jpg" alt="" class="img-responsive zoom_img_effect"></a><a href={{Route('watertreatmentchemicalsandmedia')}}>
+                <h4>Biocide & Membrane Cleaner</h4></a>
+              <p style="text-align:justify">Dynamic membrane cleaner is a highly effective cleaning solution that can remove even the most stubborn contaminants from membrane surfaces.</p>
+              <h6><a href={{Route('watertreatmentchemicalsandmedia')}}>Read more</a></h6>
+            </div>
+          </div>
+        </div>
+      
+    </section>
 </div>
+
+
+
+
+
 
 
 
